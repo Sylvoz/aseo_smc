f8a7141055692c76be98a9d6fce285125724d84d
Guillermo <guillermochacon16@gmail.com>
2023-10-20 14:18:23 -0300

    1.0

---
+++ extractor.js
@@ -0,0 +1,84 @@
+import puppeteer from "puppeteer";
+
+export async function aseo_smc(municipality,rol,dv){
+
+
+try{
+  // measurement_date info
+  const fechaActual = new Date();
+
+  const dia = fechaActual.getDate();
+  const mes = fechaActual.getMonth() + 1;
+  const año = fechaActual.getFullYear();
+  const hora = fechaActual.getHours();
+  const minutos = fechaActual.getMinutes();
+  const segundos = fechaActual.getSeconds();
+
+  const fechaFormateada = `${año}-${mes}-${dia} ${hora}:${minutos}:${segundos}`;
+
+  // Puppeteer process
+  const browser = await puppeteer.launch({
+    headless: "new",
+    defaultViewport: { width: 1280, height: 720 },
+  });
+
+  const page = await browser.newPage()
+
+  
+  await page.goto(`https://pago.smc.cl/pagoAseov2/muni/${municipality}.aspx`,{timeout:50000});
+  await page.waitForSelector('#ctl00_ContentPlaceHolder1_txtRol',{timeout:5000})
+  await page.type('#ctl00_ContentPlaceHolder1_txtRol',rol)
+  await page.type('#ctl00_ContentPlaceHolder1_txtRol2',dv)
+  await page.click('#ctl00_ContentPlaceHolder1_btnAceptar')
+
+
+  try{
+    await page.waitForSelector('#ctl00_ContentPlaceHolder1_lblTextHelp',{timeout:5000})
+    return {data:[{
+      invoice_amount: "Predio no existe",
+    }]}
+  } catch{
+    
+  }
+
+  try{
+  await page.waitForSelector('#ctl00_ContentPlaceHolder1_UpdatePanel2 > table > tbody > tr:nth-child(2) > td:nth-child(6)',{timeout:5000})
+
+  
+  const result= await page.evaluate(() =>{
+    total=document.getElementsByClassName('lblTres')[0].innerText
+    return total
+  })
+
+  let total=result
+  if (total != '$'){
+    total=total.replace('$','').replace('.','')
+    total=parseInt(total)
+  } else {
+    total=0
+  }
+
+  await browser.close()
+
+  if(total >=0){
+    return { data:[{
+      id:rol+'-'+dv,
+      measurement_date:fechaFormateada,
+      invoice_amount: total,
+    }]}
+  }
+
+  } catch {
+    return {data:[{
+      invoice_amount: "Error al cargar página",
+    }]}
+  }}
+  catch{
+    return {data:[{
+      invoice_amount: "Error al cargar página",
+    }]}
+  }
+  
+}
+
+export default aseo_smc 
---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import aseo_smc from './extractor.js';
+import {query,validationResult} from 'express-validator'
+
+const app = express();
+
+app.use(express.json());
+app.disable("x-powered-by");
+
+// Route
+app.get("/extractor",query("id").notEmpty(),query("municipality").notEmpty(),
+  async (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      const data = req.query.id;
+      const rol = data.substring(0,data.indexOf('-'))
+      const dv= data.substring(data.indexOf('-')+1,data.length)
+      const municipality= req.query.municipality.toLowerCase()
+      if (rol == "" || dv ==""){
+        return res.status(400).send({ id: "Error en rol" })
+      }
+      const total = await aseo_smc(municipality,rol, dv);
+      const {invoice_amount }= total.data[0]
+      if (invoice_amount == "Error al cargar página"){
+        res.status(500).send(JSON.stringify(total))
+      }else {
+        res.status(200).send(JSON.stringify(total));
+      }
+    } else {
+      return res.status(400).send({ id: "Falta rol o municipalidad" });
+    }
+  }
+);
+
+// PORT
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server hosted on ${PORT}`);
+});
